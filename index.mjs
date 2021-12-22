1695605c8f6dec0bd70cdfc5f6689c6662796184
papiando <info@papiando.com>
2021-12-21 22:19:02 -0400

    Progress with Server and Application

--- index.mjs
+++ index.mjs
@@ -1,5 +1,10 @@
 import Application from './lib/Handler/Application.mjs';
+import Router from './lib/Handler/Router.mjs';
 
 let app = new Application();
 
-app.handler({}, {}, {});
+app.use(Router);
+
+//app.server({ secure: true, port: 8443, cert: './.cert/cert.pem', key: './.cert/privkey.pem' })
+app.server()
+  .then((data) => console.log(data));
