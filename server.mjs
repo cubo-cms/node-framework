d05824d003f733edbe9657ae4e5211207174cfe1
papiando <info@papiando.com>
2021-12-22 18:31:46 -0400

    Minor changes

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
 /** @package        @cubo-cms/node-framework
   * @module         /server.mjs
-  * @version        0.4.38
+  * @version        0.5.39
   * @copyright      2021 Cubo CMS <https://cubo-cms.com/COPYRIGHT.md>
   * @license        ISC license <https://cubo-cms.com/LICENSE.md>
   * @author         Papiando <info@papiando.com>
@@ -11,8 +11,6 @@ await Cubo.load();
 
 const { Application } = Cubo;
 
-new Application().error((error) => {
-  console.error(error);
-}).success((app) => {
-  app.server();
-});
+const app = new Application('$/application.json');
+
+app.server();
